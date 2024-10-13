87057df9fa24cabb23d9107c14d9fc618a161e0c
cs-mshr <cs-mshr@gmail.com>
2024-10-14 00:28:45 +0530

    Add custom middleware for calculating request.user

--- logistics_provider_backend/middleware.py
+++ logistics_provider_backend/middleware.py
@@ -1,5 +1,23 @@
 from django.utils.deprecation import MiddlewareMixin
+from django.contrib.auth.models import AnonymousUser
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class DisableCSRF(MiddlewareMixin):
     def process_request(self, request):
         setattr(request, '_dont_enforce_csrf_checks', True)
+
+class CustomAuthenticationMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        auth_header = request.META.get('HTTP_AUTHORIZATION')
+        if auth_header and auth_header.startswith('Bearer '):
+            token_key = auth_header.split(' ')[1]
+            try:
+                token = Token.objects.get(key=token_key)
+                request.user = token.user
+            except Token.DoesNotExist:
+                request.user = AnonymousUser()
+        else:
+            request.user = AnonymousUser()
